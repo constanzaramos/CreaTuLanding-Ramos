ba8ef2c9db90f09138c8598f25a911d6257a3d23
Constanza Ramos <c.ramosgamboa@gmail.com>
2025-05-26 18:57:21 -0400

    icon with react icons

--- ecommerce/src/components/CartWidget/CartWidget.jsx
+++ ecommerce/src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,13 @@
 import "../CartWidget/cartWidget.scss";
+import { FaCartShopping } from "react-icons/fa6";
+
 const Cart = () => {
   return (
-    <div className="cart-widget">
-      <i class="bx  bx-cart bx-sm"></i>
-      <span className="cart-count">0</span>
+    <div>
+      <button className="cart-widget">
+        <FaCartShopping  className="cart-icon"/>
+        <span className="cart-count">0</span>
+      </button>
     </div>
   );
 };
