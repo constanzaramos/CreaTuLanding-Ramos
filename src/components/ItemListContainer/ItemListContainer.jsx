ad204556cf8b948315320d6cfdaa39a4f299e2c9
Constanza Ramos <c.ramosgamboa@gmail.com>
2025-05-28 09:53:31 -0400

    map product data and styles

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,19 @@
+import { getProducts } from '../../mock/CatalogMock';
 import '../ItemListContainer/itemListContainer.scss';
+import { useEffect, useState } from 'react';
+import ItemList from '../ItemList';
 
 const ItemListContainer = ({greeting}) => {
+    const[data, setData] = useState([]);
+    useEffect(() => {
+        getProducts()
+        .then((response) => setData(response))
+        .catch((error) => console.error("Error fetching products:", error));
+    }, []);
     return (
         <div className="item-list-container">
             <h2>{greeting} </h2>
+            <ItemList data={data} />
         </div>
         
     );
